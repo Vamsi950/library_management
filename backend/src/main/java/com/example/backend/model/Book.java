45d74741417dd3bc18169a6addb21fe78dced25d
Eshwar <143584428+Eshwar954@users.noreply.github.com>
2026-05-12 12:54:44 +0530

    Revert " updated repository"

--- backend/src/main/java/com/example/backend/model/Book.java
+++ backend/src/main/java/com/example/backend/model/Book.java
@@ -1,18 +1,5 @@
 package com.example.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
-
-@Entity
-@Data
 public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long bookId;
-    private String title;
-    private String author;
-    private boolean availability = true;
+    
 }
