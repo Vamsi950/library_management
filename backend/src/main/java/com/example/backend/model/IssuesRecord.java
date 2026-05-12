6aede84dd90dd5fb91f09b4def3415c248e47a73
Vamsi950 <vamsi950@gmail.com>
2026-05-12 13:00:12 +0530

    updated repository

--- backend/src/main/java/com/example/backend/model/IssuesRecord.java
+++ backend/src/main/java/com/example/backend/model/IssuesRecord.java
@@ -1,24 +1,6 @@
 package com.example.backend.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import java.time.LocalDate;
 
-@Entity
-@Data
 public class IssuesRecord {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long issueId;
-    
-    private LocalDate issueDate;
-    private LocalDate returnDate;
-
-    @ManyToOne
-    @JoinColumn(name = "book_id")
-    private Book book;
-
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
+   
 }
